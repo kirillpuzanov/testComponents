8ea738f5c4e509429f8fbfa247ab6ea83c3ee47f
kirillpuzanov <kirillpuzanov@mail.ru>
2021-03-10 01:52:42 +0300

    scroll

--- src/components/scroll/ScrollComp.tsx
+++ src/components/scroll/ScrollComp.tsx
@@ -7,38 +7,33 @@ type objType = {
 }
 
 export const ScrollComp = () => {
-    const newObj = [] as objType[];
+    const newArr = [] as objType[];
     const [data, setData] = useState(generateArray());
     const [resultArr, setResultArr] = useState<objType[]>([]);
     useEffect(() => {
         plus20(resultArr, data);
-    }, []);
+    }, [data, resultArr]);
 
     function plus20(arr1res: objType[], arr2Date100: objType[]) {
         let tempArr = arr2Date100.slice(arr1res.length, arr1res.length + 20);
-
-        for (let i = 0; i < tempArr.length; i++) {
-            setResultArr((arr1res) => [...arr1res, tempArr[i]]);
-        }
+        setResultArr((arr1res) => [...arr1res, ...tempArr]);
     }
 
     function generateArray() {
         const names = ['Fariz', 'Falisha', 'Mami', 'Defina', 'Fiska', 'Papi'];
         for (let i = 0; i < 100; i++) {
-            newObj[i] = {
+            newArr[i] = {
                 name: names[(Math.floor(Math.random() * (names.length)))],
                 surname: names[(Math.floor(Math.random() * (names.length)))] + Math.random().toString(36).substr(2, 3),
             };
         }
-        return newObj;
+        return newArr;
     }
 
     const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
         const element = e.currentTarget;
-
         if (resultArr.length <= 100) {
             if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) < 1) {
-                console.log('scroll');
                 plus20(resultArr, data);
             }
         }
@@ -48,7 +43,7 @@ export const ScrollComp = () => {
         <div className={st.scrollBlock} onScroll={scrollHandler}>
             {
                 resultArr.map((m, index) =>
-                <div key={index} className={st.magic}>{`${m.name} ${m.surname}`}</div>)
+                    <div key={index} className={st.magic}>{`${m.name} ${m.surname}`}</div>)
             }
         </div>
     )
